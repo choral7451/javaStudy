81f2654d62fc733bdddf1761ff73af2abbdcfc2b
Administrator <Administrator@804-21>
2022-01-12 15:41:52 +0900

    input commit

--- src/exam/Operand.java
+++ src/exam/Operand.java
@@ -3,8 +3,16 @@ package exam;
 public class Operand {
 
 	public static void main(String[] args) {
-		//연산자와 항
-		//항 (operand) : 연산에 사용되는 값
-		//연산자 (operator) : 항을 이용하여 연산하는 기호
+		// 연산자와 항
+		// 항 (operand) : 연산에 사용되는 값
+		// 연산자 (operator) : 항을 이용하여 연산하는 기호
+		
+		// 연산자의 종류
+		// 1. 산술연산자
+		// 2. 대입연산자
+		// 3. 증감연산자
+		// 4. 비교연산자
+		// 5. 논리연산자
+		// 6. 삼항연산자, instanceof 연산자
 	}
 }
